04f961a56eaeb4dd8d38e26075b42a3cc062a8ec
Daniel Palomino <dapalominop@gmail.com>
2018-09-19 11:30:23 -0500

    Modify variables of convolution gpu.

--- Cuda/Filter/Filter.cu
+++ Cuda/Filter/Filter.cu
@@ -63,19 +63,19 @@ __global__ void runConvolutionGPU(double* image, double* result, int height, int
 
 extern "C" void convolutionGPU(double* image, double* result, int x_length, int y_length, int step)
 {
-	double* dev_image, *dev_result;
+	//double* dev_image, *dev_result;
 
-	cudaMalloc((void**)&dev_image, x_length*y_length*sizeof(double));
-	cudaMalloc((void**)&dev_result, x_length*y_length*sizeof(double));
+	//cudaMalloc((void**)&dev_image, x_length*y_length*sizeof(double));
+	//cudaMalloc((void**)&dev_result, x_length*y_length*sizeof(double));
 
-	cudaMemcpy(dev_image, image, x_length*y_length*sizeof(double), cudaMemcpyHostToDevice);
+	//cudaMemcpy(dev_image, image, x_length*y_length*sizeof(double), cudaMemcpyHostToDevice);
 
 	dim3 blocks(y_length/O_TILE_HEIGHT + (((y_length%O_TILE_HEIGHT)==0)?0:1), x_length/O_TILE_HEIGHT + (((y_length%O_TILE_HEIGHT)==0)?0:1));
 	dim3 threads(BLOCK_DIM_Y,BLOCK_DIM_X);
-	runConvolutionGPU<<<blocks,threads>>>(dev_image, dev_result, y_length, x_length, step);
+	runConvolutionGPU<<<blocks,threads>>>(image, result, y_length, x_length, step);
 
-	cudaMemcpy(result, dev_result, x_length*y_length*sizeof(double), cudaMemcpyDeviceToHost);
+	//cudaMemcpy(result, dev_result, x_length*y_length*sizeof(double), cudaMemcpyDeviceToHost);
 
-	cudaFree(dev_image);
-	cudaFree(dev_result);
+	//cudaFree(dev_image);
+	//cudaFree(dev_result);
 }
